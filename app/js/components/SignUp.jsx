e1b059da46699785610b5e87abaa8ad22ca21992
Rima Gaikwad <rimarg@thoughtworks.com>
2016-11-15 18:18:13 +0530

    himanshu/rima | added two way binding for form fields using react-addons-linked-state-mixin

--- app/js/components/SignUp.jsx
+++ app/js/components/SignUp.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import utils from '../utils/utils';
 import {FormGroup, FormControl, Button} from 'react-bootstrap';
+import LinkedStateMixin from 'react-addons-linked-state-mixin' // ES6
 
 const SignUp = React.createClass({
+    mixins: [LinkedStateMixin],
+    getInitialState: function() {
+        return {
+            email: '',
+            name: '',
+            password: '',
+            mobileNumber: '',
+            city: ''
+        }
+    },
     signUp() {
         utils.register();
         browserHistory.push('/');
@@ -15,19 +26,19 @@ const SignUp = React.createClass({
                     Create an account
                 </FormGroup>
                 <FormGroup>
-                    <FormControl type="text" id="name" placeholder="Name"/>
+                    <FormControl type="text" id="name" placeholder="Name" valueLink={this.linkState('name')}/>
                 </FormGroup>
                 <FormGroup>
-                    <FormControl type="text" id="email-id" placeholder="Email"/>
+                    <FormControl type="email" id="email-id" placeholder="Email" valueLink={this.linkState('email')}/>
                 </FormGroup>
                 <FormGroup>
-                    <FormControl type="password" id="password" placeholder="Password"/>
+                    <FormControl type="password" id="password" placeholder="Password" valueLink={this.linkState('password')}/>
                 </FormGroup>
                 <FormGroup>
-                    <FormControl type="text" id="mobile-number" placeholder="Mobile Number"/>
+                    <FormControl type="text" id="mobile-number" placeholder="Mobile Number" valueLink={this.linkState('mobileNumber')}/>
                 </FormGroup>
                 <FormGroup>
-                    <FormControl type="text" id="city" placeholder="City"/>
+                    <FormControl type="text" id="city" placeholder="City" valueLink={this.linkState('city')}/>
                 </FormGroup>
                 <FormGroup>
                     <Button onClick={this.signUp}>Create an account</Button>
