9cce8d821285bef310fad0aeecf12e322bdc23af
mehrotras <msahil@thoughtworks.com>
2016-11-15 13:44:44 +0530

    [sahil] -- revert my changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Routes from './app/js/routes'
-import PostAdForm from './app/js/components/PostAdForm'
 
-render(<PostAdForm/>, document.getElementById('app'))
+render(Routes, document.getElementById('app'))
