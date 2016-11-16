1c14b30aa36cccce7e89317deb9d6c82402e885e
mehrotras <msahil@thoughtworks.com>
2016-11-16 10:21:45 +0530

    [sahill, saurabh] test create ad

--- test/testPostAddForm.js
+++ test/testPostAddForm.js
@@ -1,11 +1,29 @@
 import { mount, shallow } from 'enzyme'
 import React from 'React'
 import {expect} from 'chai';
-import PostAdForm from '../app/js/components/PostAdForm'
+import CreateAd from '../app/js/components/CreateAd'
 
-describe('<PostAdForm/>', function () {
-    it('should have an input to get the name', function () {
-        const wrapper = shallow(<PostAdForm/>);
-        expect(wrapper.find('input')).to.have.length(2);
+describe('<CreateAd/>', function () {
+    it('should have 4 input to get the title, price, mobile-number, submit', function () {
+        const wrapper = shallow(<CreateAd/>);
+        expect(wrapper.find('input')).to.have.length(4);
     });
+
+    it('should have 2 select to get the city, category', function () {
+        const wrapper = shallow(<CreateAd/>);
+        expect(wrapper.find('select')).to.have.length(2);
+    });
+
+    it('should have a textarea to get the description', function () {
+        const wrapper = shallow(<CreateAd/>);
+        expect(wrapper.find('textarea')).to.have.length(1);
+    });
+
+    it('price should not be zero', function() {
+        const wrapper = shallow(<CreateAd/>);
+        wrapper.setState({value: -100});
+        expect(wrapper.find('#price').text()).to.contain(100);
+    });
+
+
 });
